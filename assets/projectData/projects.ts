931d549a18b30c651fcb69032a1d4f5627240ed4
paragbadala <parag.badala@gmail.com>
2020-07-06 21:14:48 +0530

    Auto-generated commit

--- assets/projectData/projects.ts
+++ assets/projectData/projects.ts
@@ -2,73 +2,73 @@ export const projectDetails = [
     {
         "projectName": "DealNoDeal",
         "projectDes": "It a lucky drawer game developed with angular with many complex algorithm applied for different situation",
-        "projectImg": "../../../assets/images/dealNoDeal.PNG",
+        "projectImg": "./assets/images/dealNoDeal.PNG",
         "projectLink": ""
     },
     {
         "projectName": "Mosam",
         "projectDes": "It is a weather forecast app with features of data analytics and creating the graphs with different paramters",
-        "projectImg": "../../../assets/images/weatherApp5.PNG",
+        "projectImg": "./assets/images/weatherApp5.PNG",
         "projectLink": ""
     },
     {
         "projectName": "MedicX",
         "projectDes": "It is an online medical report portal contains features of role base authentication, uploading and downloading the report of patients",
-        "projectImg": "../../../assets/images/medicalPortal.PNG",
+        "projectImg": "./assets/images/medicalPortal.PNG",
         "projectLink": ""
     },
     {
         "projectName": "Smart Table",
         "projectDes": "It is an angular library which takes JSON data and generate table with functionality of dynamic columns and rows, filtering, sorting, dynamic pagination",
-        "projectImg": "../../../assets/images/smartTable.PNG",
+        "projectImg": "./assets/images/smartTable.PNG",
         "projectLink": ""
     },
     {
         "projectName": "BuyOut",
         "projectDes": "It is an e-commerce website designed in vanilla javascript and used NodeJS as a backend for serving the apis.",
-        "projectImg": "../../../assets/images/eCommerce.PNG",
+        "projectImg": "./assets/images/eCommerce.PNG",
         "projectLink": ""
     },
     {
         "projectName": "Form Generator",
         "projectDes": "It is an angular application which creates a JSON object of form which is then converted into angular reactive forms ",
-        "projectImg": "../../../assets/images/dynamicForm.PNG",
+        "projectImg": "./assets/images/dynamicForm.PNG",
         "projectLink": ""
     },
     {
         "projectName": "Imtehaan",
         "projectDes": "It is a eLearning test platform with user and admin portals of taking and giving the online test, contains security features",
-        "projectImg": "../../../assets/images/mcqPortal2.PNG",
+        "projectImg": "./assets/images/mcqPortal2.PNG",
         "projectLink": ""
     },
     {
         "projectName": "Portfolio",
         "projectDes": "My portfolio site contains detail description about me and my projects. Built using angular framework.",
-        "projectImg": "../../../assets/images/portfolio.png",
+        "projectImg": "./assets/images/portfolio.png",
         "projectLink": ""
     },
     {
         "projectName": "Movie Store",
         "projectDes": "It is a movie database which help user to maintain its movie collection records and is developed using angular",
-        "projectImg": "../../../assets/images/furious-fan.PNG",
+        "projectImg": "./assets/images/furious-fan.PNG",
         "projectLink": ""
     },
     {
         "projectName": "Webcrawler",
         "projectDes": "It is WebCrawler which will help in making the sitemap for the website which is basically all the links and pages of the websites.",
-        "projectImg": "../../../assets/images/webCrawler.png",
+        "projectImg": "./assets/images/webCrawler.png",
         "projectLink": ""
     },
     {
         "projectName": "Spychat",
         "projectDes": "It is a python project in which we can send message encoded behind the image and the reciever can decode that message.",
-        "projectImg": "../../../assets/images/spyChat.png",
+        "projectImg": "./assets/images/spyChat.png",
         "projectLink": ""
     },
     {
         "projectName": "Instabot",
         "projectDes": "Terminal bot app using python that allows you to use the various features of the Instagram like, comments, post.",
-        "projectImg": "../../../assets/images/instaBot1.jpg",
+        "projectImg": "./assets/images/instaBot1.jpg",
         "projectLink": ""
     }
 ]
